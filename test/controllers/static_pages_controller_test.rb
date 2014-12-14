92ad2da00309c42a56852363673c3c04846b19d6
LinuxGit <gofaceme@gmail.com>
2014-12-14 12:20:12 +0800

    Improve test: setup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,31 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "New Weibo"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | New Weibo"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | New Weibo"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "shoud get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | New Weibo"
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "shoud get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
